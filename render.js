badee5f369bd87444a4c1ac3f2c6504aad76df59
Simon Kalt <kaltsimon@users.noreply.github.com>
2016-06-21 20:38:10 +0200

    Initial commit

---
+++ render.js
@@ -0,0 +1,7 @@
+const $ = require('jquery');
+const shell = require('electron').shell;
+
+$(document).on('click', 'a[href^="http"]', function(event) {
+    event.preventDefault();
+    shell.openExternal(this.href);
+});
